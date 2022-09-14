8f5ae20bc8a3eea0653f0341540cfcba8dc0cf61
NidhiSharma63 <nidhisharma639593@gmail.com>
2022-09-14 14:30:49 +0530

    monkey thought shows on wrong email

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const monkeyFace = document.querySelector('.monkey-face');
 const monkeyHand = document.querySelector('.monkey-hand');
-const input = document.getElementById('input');
+const email = document.getElementById('email');
+const monkeyThought = document.querySelector('.monkey-thought');
+const monkeyEyesBrows = document.querySelectorAll('.eye-brow')
+
+const mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 
 let degree = 13
 let inputPrevLenght = [];
@@ -14,12 +18,16 @@ document.addEventListener('click',(e)=>{
     monkeyHand.style.transform='translateY(120%)'
   }
   if(e.target.type!=='email'){
-   monkeyFace.style.transform = `perspective(800px) rotateZ(0deg)`
+   monkeyFace.style.transform = `perspective(800px) rotateZ(0deg)`;
+   monkeyEyesBrows.forEach((eyeBrow)=>{
+    eyeBrow.style.transform = 'translateY(-2px)'
+  })
   }
+  
 });
 
-
-input.addEventListener('input',(e)=>{
+// move monkey face
+email.addEventListener('input',(e)=>{
   let currentInputLength = String(e.target.value).length;
   let decrementInInputValue = inputPrevLenght.includes(currentInputLength);
   if(!decrementInInputValue && degree>= -10){
@@ -29,5 +37,17 @@ input.addEventListener('input',(e)=>{
   if(decrementInInputValue && degree<13){
     degree+=1
   }
+  if(!email.value.match(mailformat)){
+    monkeyThought.style.opacity='1';
+      monkeyEyesBrows.forEach((eyeBrow)=>{
+        eyeBrow.style.transform = 'translateY(3px)'
+      })
+  };
+  if(email.value.match(mailformat)){
+    monkeyThought.style.opacity='0';
+    monkeyEyesBrows.forEach((eyeBrow)=>{
+      eyeBrow.style.transform = 'translateY(-3px)'
+    })
+  };
   monkeyFace.style.transform = `perspective(800px) rotateZ(${degree}deg)`
-})
+});
